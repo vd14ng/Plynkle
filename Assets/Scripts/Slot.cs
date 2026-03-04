cb859127682b8b6a684a127cf7485d9c92074976
Van Duong <vqduong2024@gmail.com>
2026-03-04 10:08:42 -0800

    Added ScoreKeeper class.

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Slot : MonoBehaviour
 {
+    public int Points = 10;
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("SlotTrigger"))
@@ -9,5 +10,16 @@ public class Slot : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    
+
+    private void OnBallEntered(Collider2D other)
+    {
+        ScorePoints();
+    }
+
+    private void ScorePoints()
+    {
+        // tell the scorekeeper to add some amount of points
+        
+        
+    }
 }
