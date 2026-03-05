69b418faea19260e6e8ab98858238cf7e4a2c58e
Van Duong <vqduong2024@gmail.com>
2026-03-04 16:58:28 -0800

    Added fireworks and firework sounds.

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -4,6 +4,9 @@ public class Slot : MonoBehaviour
 {
     public UI Ui;
     public int points = 10;
+    public GameObject FireworksParticlePrefab;
+    public Sounds Sounds;
+    
     public void OnTriggerEnter2D(Collider2D other)
     {
         // print("OnTriggerEnter2D");
@@ -15,11 +18,31 @@ public class Slot : MonoBehaviour
     }
     
     // when ball enters the slot, you score points
-    private void OnBallEntered(Collider2D other)
+    private void OnBallEntered(Collider2D ball)
     {
-        // print("OnBallEntered");
         ScorePoints();
+        SpawnParticles(ball);
+        PlayFireworksSound();
     }
+
+    private void PlayFireworksSound()
+    {
+        Sounds.PlayFireworksSound();
+    }
+
+    private void SpawnParticles(Collider2D ball)
+    { 
+        Vector3 particlePosition = GetParticlePosition(ball); 
+        Instantiate(FireworksParticlePrefab, particlePosition, Quaternion.identity);
+    }
+
+    private Vector3 GetParticlePosition(Collider2D ball)
+    {
+        Vector3 ballPosition = ball.transform.position;
+        ballPosition.y = ballPosition.y + 0.25f;
+        return ballPosition;
+    }
+
     private void ScorePoints()
     {
         // tell the scorekeeper to add some amount of points
